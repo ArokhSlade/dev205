2f3499b0f1e00e67c0f071e143efeece529f9fa7
Gerald Schmidtke <gerald.schmidtke@s4g.school4games.net>
2025-05-27 10:34:00 +0200

    PlayerInput script with member variables

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField, Header("Input Action Asset")]
+    private InputActionAsset playerActionAsset;
+    [SerializeField]
+    private string playerActionMapName;
+    [SerializeField]
+    private string mousePositionActionName;
+    [SerializeField]
+    private string mouseInteractionActionName;
+
+    private InputAction mousePositionAction;
+    private InputAction mouseInteractionAction;
+}
