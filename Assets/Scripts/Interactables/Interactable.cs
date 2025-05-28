11024c893d75827e79c6ab15d07f245c3b165001
Gerald Schmidtke <gerald.schmidtke@s4g.school4games.net>
2025-05-28 12:36:51 +0200

    interactable base code

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -2,8 +2,31 @@
 
 namespace DEV205.Interactables
 {
+    [RequireComponent(typeof(SphereCollider))]
     public class Interactable : MonoBehaviour
     {
+        [SerializeField] private float debugInteractionRadius = 1f;
+        [SerializeField] private Color debugColor = Color.cyan;
 
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player")) return;
+            Interact();
+        }
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = debugColor;
+            Gizmos.DrawWireSphere(transform.position, debugInteractionRadius);
+        }
+
+        private void OnValidate()
+        {
+            GetComponent<SphereCollider>().radius = debugInteractionRadius;
+        }
+
+        protected virtual void Interact()
+        {
+
+        }
     }
 }
