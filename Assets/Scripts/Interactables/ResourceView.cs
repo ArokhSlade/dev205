b16dcd256ecd088b21f8ebe22a6dea8c7db41084
Gerald Schmidtke <gerald.schmidtke@s4g.school4games.net>
2025-05-28 15:44:29 +0200

    hud layout

--- Assets/Scripts/Interactables/ResourceView.cs
+++ Assets/Scripts/Interactables/ResourceView.cs
@@ -1,4 +1,36 @@
-﻿namespace DEV205.Interactables
-{
+﻿using System;
+using UnityEngine;
 
+namespace DEV205.Interactables
+{
+    public class ResourceView : MonoBehaviour
+    {
+        [SerializeField]
+        private ResourceType resourceType;
+
+        private void OnEnable()
+        {
+            Inventory.ResourceChanged += UpdateView;
+            UpdateView();
+        }
+
+        private void UpdateView()
+        {
+            uint amount = Inventory.Instance.GetResourceValue(resourceType);
+            // set value to text display
+        }
+        private void UpdateView(ResourceType type, uint arg2)
+        {
+            if (this.resourceType == type)
+            {
+                // set value to text display
+            }
+        }
+
+
+        private void OnDisable()
+        {
+            Inventory.ResourceChanged -= UpdateView;
+        }
+    }
 }
